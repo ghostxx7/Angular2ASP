5b3de61cb711b167cdfb91f6d67bbcd9dc0e78d3
ghostxx7 <ghostxx7@gmail.com>
2016-04-06 13:06:13 +0300

    2

---
+++ src/Angular2ASP/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+
+namespace Angular2ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/Angular2ASP/Startup.cs
+++ src/Angular2ASP/Startup.cs
@@ -1,34 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Glimpse;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 
 namespace Angular2ASP
 {
     public class Startup
     {
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddGlimpse();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env, 
+                              ILoggerFactory loggerFactory)
         {
-            app.UseIISPlatformHandler();
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-            app.Run(async (context) =>
+            if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseIISPlatformHandler(options => 
+                                      options.AuthenticationDescriptions.Clear());
+            app.UseStaticFiles();
+            app.UseGlimpse();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", 
+                                "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute("spa-fallback", 
+                                "{*anything}", 
+                                new { controller = "Home", action = "Index" });
+                routes.MapWebApiRoute("defaultApi", 
+                                      "api/{controller}/{id?}");
             });
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
